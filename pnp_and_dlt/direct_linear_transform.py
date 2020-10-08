46301459bac8c10ee5c25d15ef91bf67681399da
yardenas <yardas@student.ethz.ch>
2020-10-08 15:57:15 +0200

    Reprojection working

--- pnp_and_dlt/direct_linear_transform.py
+++ pnp_and_dlt/direct_linear_transform.py
@@ -3,18 +3,15 @@ import numpy as np
 
 class DirectLinearTransform(object):
     def __init__(self, calibration_matrix, points_world):
-        self._k_inv = np.linalg.inv(np.array(calibration_matrix))
+        self._k_inv = np.linalg.inv(calibration_matrix)
         self._k = np.array(calibration_matrix)
         self._points_world = points_world
 
     def estimate_pose_dlt(self, points_camera):
         points_camera_calibrated = self._k_inv.dot(points_camera.T).T
-        assert points_camera_calibrated.shape == points_camera.shape, "Fucked up points normalization."
-        assert points_camera_calibrated.shape == self._points_world.shape, "Input shape mismatch"
         x_mul = np.hstack([self._points_world,
                            np.zeros_like(self._points_world),
                            -(self._points_world.T * points_camera_calibrated[:, 0]).T])
-
         y_mul = np.hstack([np.zeros_like(self._points_world),
                            self._points_world,
                            -(self._points_world.T * points_camera_calibrated[:, 1]).T])
@@ -26,10 +23,11 @@ class DirectLinearTransform(object):
         alpha_m = vh[-1, :].reshape((3, 4)) * np.sign(vh[-1, -1])
         r = alpha_m[:3, :3]
         u, _, vh_r = np.linalg.svd(r)
-        r_tilde = u * vh_r
+        r_tilde = u.dot(vh_r)
         alpha = np.linalg.norm(r_tilde) / np.linalg.norm(r)
-        return np.hstack([r_tilde, np.expand_dims(alpha_m[:-1, -1], axis=1) / alpha])
+        return np.concatenate([r_tilde, np.expand_dims(alpha_m[:, -1], axis=1) * alpha], axis=1)
 
     def reproject_points(self, points_camera):
         projection_matrix = self.estimate_pose_dlt(points_camera)
-        return (self._k.dot(projection_matrix)).dot(self._points_world.T).T
+        reprojected_points = self._k.dot(projection_matrix.dot(self._points_world.T)).T
+        return reprojected_points / np.expand_dims(reprojected_points[:, -1], axis=1)
--- pnp_and_dlt/pose_estimation_script.py
+++ pnp_and_dlt/pose_estimation_script.py
@@ -1,20 +1,39 @@
 import os
 import time
 import re
+import argparse
+
+import cv2 as cv
+import numpy as np
 
 import pnp_and_dlt.utils as utils
 from pnp_and_dlt.direct_linear_transform import DirectLinearTransform
 
 
+def draw_points_on_image(image, camera_points, reprojected_points):
+    for reprojected_point, camera_point in zip(reprojected_points, camera_points):
+        cv.circle(image, tuple(reprojected_point[:-1].astype(np.int)), 2, (0, 0, 255))
+        cv.circle(image, tuple(camera_point[:-1].astype(np.int)), 3, (0, 255, 0))
+    cv.imshow("Re-projected points", image)
+    cv.waitKey(1)
+
+
 def main():
-    calibration_mat, points_world, points_camera = utils.load_data('data')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no_visualization', action='store_true')
+    parser.add_argument('--data_dir', type=str, default='data')
+    args = parser.parse_args()
+    calibration_mat, points_world, points_camera = utils.load_data(args.data_dir)
+    print("Re-projecting points in path: '{}' using camera calibration matrix {}"
+          .format(args.data_dir, calibration_mat))
     dlt = DirectLinearTransform(calibration_mat, points_world)
-    for image, image_name in utils.images(os.path.join('data', 'images_undistorted')):
+    for image, image_name in utils.images(os.path.join(args.data_dir, 'images_undistorted')):
         image_id = int(re.sub("[^0-9]", "", image_name)) - 1
         reprojected_points = dlt.reproject_points(points_camera[image_id, ...])
-
+        if not args.no_visualization:
+            draw_points_on_image(image, points_camera[image_id, ...], reprojected_points)
         time.sleep(33 / 1000)
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
